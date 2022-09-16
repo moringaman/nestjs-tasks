d8bdad018a770b8c39b7539a14ce84f91b0bb413
Rob (Webnostix) <rob@webnostix.co.uk>
2022-09-16 14:03:01 +0000

    env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
-
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TasksModule,
     AuthModule],
   controllers: [],
